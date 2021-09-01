53fe02f7d0250a7fbd7d43841ee68bd7081c9b24
Nick Corin <nickcorin@gmail.com>
2021-09-01 11:56:30 +0200

    substate: Remove testing constraint in Substate constructor

--- template.go
+++ template.go
@@ -12,9 +12,7 @@ import (
 )
 {{end}}
 
-// NewSubstateForTesting returns an implementation of Substate which can be used
-// for testing.
-func NewSubstateForTesting(_ *testing.TB, injectors...Injector) *substate {
+func NewSubstate(injectors...Injector) *substate {
 	var s substate
 
 	for _, injector := range injectors {
@@ -37,7 +35,6 @@ func (fn InjectorFunc) Inject(s *substate) {
 	fn(s)
 }
 {{range $index, $field := .Fields}}
-// With{{$field.Method}} returns an Injector which sets the {{$field.Name}} on substate.
 func With{{$field.Method}}({{$field.Name}} {{$field.Type}}) InjectorFunc {
 	return func(s *substate) {
 		s.{{$field.Name}} = {{$field.Name}}
@@ -50,7 +47,6 @@ type substate struct {
 {{- end}}
 }
 {{range $index, $field:= .Fields}}
-// {{$field.Method}} implements the Substate interface.
 func (s *substate) {{$field.Method}}{{$field.Params}} {{$field.Results}} {
 	return s.{{$field.Name}}
 }
